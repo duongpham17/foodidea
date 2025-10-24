6c3f880f048242322b3ba5e395661195b62ea62a
duongpham17 <duongpham17@icloud.com>
2025-10-24 21:34:54 +0100

    first commit

--- src/app/api/public/profile/[id]/route.ts
+++ src/app/api/public/profile/[id]/route.ts
@@ -3,15 +3,26 @@ import connectDB from '@database/connect';
 import recipesMODEL from '@database/models/recipes';
 import usersMODEL from '@database/models/users';
 
-async function get(req: NextRequest, {params}: {params: {id: string}}) {
+export async function GET(req: NextRequest, context: { params: Promise<{ id: string }> }) {
   try {
     await connectDB();
-    const [ page, limit ] = [ Number(req.nextUrl.searchParams.get("page")), Number(req.nextUrl.searchParams.get("limit")) ];
-    const recipes = await recipesMODEL.find({user: params.id}).sort({timestamp: -1}).skip((page-1)*limit).limit(limit);
+
+    const params = await context.params;
+    
+    const page = Number(req.nextUrl.searchParams.get("page") ?? 1);
+    const limit = Number(req.nextUrl.searchParams.get("limit") ?? 10);
+
+    const recipes = await recipesMODEL
+      .find({ user: params.id })
+      .sort({ timestamp: -1 })
+      .skip((page - 1) * limit)
+      .limit(limit);
+
     const user = await usersMODEL.findById(params.id, "username recipes favourites");
-    return NextResponse.json({ status: "success", data: {recipes, user} }, { status: 200 });
+
+    return NextResponse.json({ status: "success", data: { recipes, user } }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ status: "failed", message: "Internal server error" }, { status: 500 });
   }
 };
-export const GET = get;
+
--- src/app/api/public/recipes/[id]/route.ts
+++ src/app/api/public/recipes/[id]/route.ts
@@ -3,9 +3,10 @@ import connectDB from '@database/connect';
 import recipesMODEL from '@database/models/recipes';
 import usersMODEL from '@database/models/users';
 
-async function get(req: NextRequest, {params}: {params: {id: string}}) {
+export async function GET(req: NextRequest, context: { params: Promise<{ id: string }> }) {
   try {
     await connectDB();
+    const params = await context.params;
     const recipes = await recipesMODEL.findByIdAndUpdate(params.id, {$inc: {views: 1}}, {new: true});
     if(!recipes) return NextResponse.json({ status: "failed", message: "Internal server error" }, { status: 500 });
     const user = await usersMODEL.findById(recipes.user, 'username').lean();
@@ -14,4 +15,3 @@ async function get(req: NextRequest, {params}: {params: {id: string}}) {
     return NextResponse.json({ status: "failed", message: "Internal server error" }, { status: 500 });
   }
 };
-export const GET = get;
